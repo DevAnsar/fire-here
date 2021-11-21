951e3761371a379d3ed02b81b4ba7e7418ad80d7
Ansar <ansarmirzayi@gmail.com>
2021-11-22 02:27:40 +0330

    update package to class-base component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {FireHere} from './lib/index'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" >
+      <div className="space" style={{height:800}} />
+        <FireHere id='test1' onHereFn={()=>console.log('run')} >fire-here</FireHere>
+        <div className="space" style={{height:1000}} />
+
+        <span>hi , my here</span>
+        <FireHere id='test2' onHereFn={()=>alert('hi , my here')} />
+        <div  className="space" style={{height:1000}} />
         
       </header>
     </div>
--- src/lib/components/FireHere.js
+++ src/lib/components/FireHere.js
@@ -1,11 +1,33 @@
-import React, { useState } from "react";
+import React  from "react";
+class FireHere extends React.Component{
+    constructor(){
+        super();
+            this.state={
+                usedFire : false 
+            }
+            this.eleRef = React.createRef();
+    }
+    componentDidMount(){
+        document.addEventListener('scroll',()=>{
+            if(this.getScrollY() + this.getClientHeight() >= this.getOffsetTop()){
+                if(! this.state.usedFire){
+                    this.setState({usedFire:true});
+                    this.props.onHereFn()
+                }
+                
+            }
+        })
+    }
+        getClientHeight =()=>document.documentElement.clientHeight;
+        getScrollY =()=>window.scrollY;
+        getOffsetTop =()=>this.eleRef.current.offsetTop;
 
-function FireHere({onHereFn,childer}){
-    return(
-        <div >
-            <button onClick={onHereFn} >من اینجام</button>
-            {childer}
-        </div>
-    )
+        render(){
+            return(
+                <div id={this.props.id} ref={this.eleRef}>
+                    {this.props.childer}
+                </div>
+            )
+        }
 }
 export default FireHere;
